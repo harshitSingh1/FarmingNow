3966bb2c5105efbc6eafe1ec45bfca1458e9af91
Harshit Singh <95095505+harshitSingh1@users.noreply.github.com>
2024-09-15 16:14:56 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
 // server.js
-
+const http = require('http');
 const app = require('./app');
 const port = process.env.PORT || 5000;
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+const server = http.createServer(app);
+server.listen(port,()=>{console.log('this app is running on '+port)});
